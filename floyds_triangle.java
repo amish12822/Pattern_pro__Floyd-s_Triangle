cfc983f2089cea5854193d4f1006dbf50011bc1c
amish12822 <amishofficial23@gmail.com>
2024-07-27 21:07:39 +0530

    complete program floyd's triangle

---
+++ floyds_triangle.java
@@ -0,0 +1,22 @@
+/**
+ * floyds_triangle
+ */
+public class floyds_triangle {
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int num = 1;
+
+        for (int i=1; i<=a; i++){
+
+            for (int j=1; j<=i; j++){
+
+                System.out.print(num + " ");
+                num++;
+            }
+
+            System.out.println();
+        }
+    }
+}
